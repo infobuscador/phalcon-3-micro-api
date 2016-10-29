356e9ca0fbffcbce832d37ec53973db35c7ecdde
Julian Gafiu <julian@iMac-de-Julian.local>
2016-10-29 23:22:44 +0200

    initial commit

---
+++ app/config/config.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * @author: Iulian Gafiu <weblysolutions@gmail.com>
+ * Project: Phalcon 3 Micro Skelenton
+ * @license: The MIT License (MIT)
+ * Copyright (c) 2016 Iulian Gafiu
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed
+ * with this source code.
+ */
+
+defined('BASE_PATH') || define('BASE_PATH', getenv('BASE_PATH') ?: realpath(dirname(__FILE__) . '/../..'));
+defined('APP_PATH') || define('APP_PATH', BASE_PATH . '/app');
+
+error_reporting(E_ALL ^ E_NOTICE);
+
+
+return new \Phalcon\Config([
+    'database' => [
+        'adapter'     => 'Mysql',
+        'host'        => 'localhost',
+        'username'    => 'root',
+        'password'    => '',
+        'dbname'      => '',
+        'charset'     => 'utf8',
+    ],
+    'application' => [
+        'appDir'         => APP_PATH . '/',
+        'controllersDir' => APP_PATH . '/controllers/',
+        'modelsDir'      => APP_PATH . '/models/',
+        'libraryDir'     => APP_PATH . '/libraries/',
+        'baseUri'        => '//api.local.dev/',
+    ]
+]);
